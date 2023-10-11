877e6d3893872870efc47244078f32d252d26f02
rovezuka <rostislav.04@list.ru>
2023-10-12 02:17:31 +0300

    Add dry-rb validation

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,18 +1,24 @@
 class PatientsController < ApplicationController
-        # Действие для создания нового пациента
-        def create
-          patient = Patient.new(patient_params)
-          if patient.save
-            render json: patient, status: :created
-          else
-            render json: patient.errors, status: :unprocessable_entity
-          end
-        end
-      
-        private
-      
-        def patient_params
-          params.require(:patient).permit(:full_name, :date_of_birth, :phone_number, :email)
-        end
+  # Действие для создания нового пациента
+  def create
+    contract = PatientContract.new
+    result = contract.call(patient_params)
+
+    if result.success?
+      patient = Patient.new(patient_params)
+      if patient.save
+        render json: patient, status: :created
+      else
+        render json: patient.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: result.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:full_name, :date_of_birth, :phone_number, :email)
+  end
 end
-      
